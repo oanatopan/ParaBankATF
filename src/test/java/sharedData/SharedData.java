38060edcdf057fc1489f9f580a49366e537ac7b2
oanatopan <19oanatopan@gmail.com>
2026-05-07 06:08:05 +0300

    Improve test reliability and add MySQL data integration

--- src/test/java/sharedData/SharedData.java
+++ src/test/java/sharedData/SharedData.java
@@ -3,6 +3,7 @@ package sharedData;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -38,6 +39,8 @@ public class SharedData {
 
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
 
         driver.get("https://parabank.parasoft.com/parabank/register.htm");
         LogUtility.infoLog("The user navigates to: " + driver.getCurrentUrl());
