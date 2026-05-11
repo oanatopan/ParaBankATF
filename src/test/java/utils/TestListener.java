5168592431a80944a80e79ab9b29c59ab96ce5ae
oanatopan <19oanatopan@gmail.com>
2026-05-11 07:58:46 +0300

    automate environment.properties copy to allure-results

--- src/test/java/utils/TestListener.java
+++ src/test/java/utils/TestListener.java
@@ -1,10 +1,18 @@
 package utils;
 
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import sharedData.SharedData;
 
-public class TestListener implements ITestListener {
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class TestListener implements ITestListener, ISuiteListener {
 
     @Override
     public void onTestFailure(ITestResult result) {
@@ -13,4 +21,20 @@ public class TestListener implements ITestListener {
             ((SharedData) currentClass).saveScreenshot();
         }
     }
+
+    @Override
+    public void onFinish(ISuite suite) {
+        try {
+            Files.createDirectories(Paths.get("target/allure-results"));
+            InputStream is = getClass().getClassLoader()
+                    .getResourceAsStream("environment.properties");
+            if (is != null) {
+                Files.copy(is, Paths.get("target/allure-results/environment.properties"),
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (IOException e) {
+            System.err.println("Could not copy environment.properties: " + e.getMessage());
+        }
+    }
+
 }
